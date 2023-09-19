40931b3eb9f5fb067a0b1d11d981fa5bb3b5abff
Vahan Virabyan <vahan.virabyan99@gmail.com>
2023-09-19 21:18:44 +0400

    homework 1

---
+++ src/classwork/chapter6/BoxDemo.java
@@ -0,0 +1,14 @@
+package classwork.chapter6;
+
+public class BoxDemo {
+//    public static void main(String[] args) {
+//        Box myBox = new Box();
+//        double vol;
+//        myBox.width = 10;
+//        myBox.depth = 20;
+//        myBox.height = 15;
+//        vol = myBox.width * myBox.height * myBox.depth;
+//        System.out.println("Объем равен " + vol);
+//
+//    }
+}
