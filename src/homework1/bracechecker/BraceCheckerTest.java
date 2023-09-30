65bece528fad2b575bc28a985f152523bcf6e4c0
Vahan Virabyan <vahan.virabyan99@gmail.com>
2023-09-30 19:55:24 +0400

    homework 1

--- src/homework1/bracechecker/BraceCheckerTest.java
+++ src/homework1/bracechecker/BraceCheckerTest.java
@@ -2,7 +2,7 @@ package homework1.bracechecker;
 
 public class BraceCheckerTest {
     public static void main(String[] args) {
-       BraceChecker bc = new BraceChecker("{sdsdsds} [[sdsdsdsd]");
+       BraceChecker bc = new BraceChecker("{Hello from [Java]sdsdsds} [[sdsdsdsd]");
        bc.check();
     }
 }
